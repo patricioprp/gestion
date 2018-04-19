f1c386a05ff3d1a7483c3552dadaeecca9f85902
patricio polito <patricioprp06@gmail.com>
2018-04-19 12:52:41 -0300

    provincias

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -2,8 +2,15 @@ $("#provincia").change(function(event){
 $.get("locations/"+event.target.value+"",function(response, provincia){
   $("#localidad").empty();
   for(i=0;i<response.length;i++){
-    $("#localidad").append("<option value='"+response[i].id+"'> "+
-    response[i].name+"</option>");
+    $("#localidad").append("<option value='"+response[i].id+"'> "+response[i].name+"</option>");
+  }
+});
+});
+$("#nacion").change(function(event){
+$.get("provinces/"+event.target.value+"",function(response, nacion){
+  $("#provincia").empty();
+  for(i=0;i<response.length;i++){
+    $("#provincia").append("<option value='"+response[i].id+"'> "+response[i].name+"</option>");
   }
 });
 });
